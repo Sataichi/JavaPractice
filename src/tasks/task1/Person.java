4a930307da9fc14d74138b19ec7ed7b7040cf871
Sataichi <bulahk3070a@gmail.com>
2020-11-16 20:06:07 +0300

    add Task 1

---
+++ src/tasks/task1/Person.java
@@ -0,0 +1,38 @@
+package tasks.task1;
+
+import java.time.Period;
+
+public class Person {
+
+	private String name;
+
+	private Period age;
+
+	public Person(String name, Period age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Period getAge() {
+		return age;
+	}
+
+	public void setAge(Period age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+}
---
+++ src/tasks/task1/README.md
@@ -0,0 +1,7 @@
+# TASK 1 
+1. Person class with name and age fields<br/>
+2. A collection of Persons;<br/>
+3. Two instances of Comparator interface using lambda expressions: first one for comparing by name, second one – by age.
+Then sort them using these comparators.
+Use forEach method for printing information about all the persons.
+Try to use method reference if it is possible.
